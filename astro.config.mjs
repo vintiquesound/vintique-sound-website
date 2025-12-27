b13bb6bd768fb725e2af05a2a66c5f7bb663bc92
Kevin Ulliac <kevanoullio@outlook.com>
2025-12-27 13:53:56 -0700

    Added tailwindcss and shadcn ui components to project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,27 @@ import path from "path";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.vintiquesound.com",
-    integrations: [
-      sitemap(),
-      react(),
-      mdx(),
-      icon(),
+  site: "https://www.vintiquesound.com",
+  integrations: [
+    sitemap(),
+    react(),
+    mdx(),
+    icon(),
+  ],
+  vite: {
+    plugins: [
+      tailwindcss()
     ],
-    vite: {
-        resolve: {
-            alias: {
-                '@': path.resolve(new URL('.', import.meta.url).pathname, "src"),
-            },
-        },
+    resolve: {
+      alias: {
+        '@': path.resolve(new URL('.', import.meta.url).pathname, "src"),
+      },
     },
+  },
 });
