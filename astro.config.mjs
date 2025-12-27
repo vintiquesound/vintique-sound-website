9a45ab8fd8eedbf8bbde8190abc309ae02a221ab
Kevin Ulliac <kevanoullio@outlook.com>
2025-12-27 12:33:29 -0700

    Fixed social links, added common icon libraries, fixed header menu, added light/dark/system modes.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
 // @ts-check
 
-import { defineConfig } from 'astro/config';
-import path from 'path';
+import { defineConfig } from "astro/config";
+import path from "path";
 
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import react from '@astrojs/react';
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.vintiquesound.com',
-    integrations: [sitemap(), react(), mdx()],
+    site: "https://www.vintiquesound.com",
+    integrations: [
+      sitemap(),
+      react(),
+      mdx(),
+      icon(),
+    ],
     vite: {
         resolve: {
             alias: {
-                '@': path.resolve(new URL('.', import.meta.url).pathname, 'src'),
+                '@': path.resolve(new URL('.', import.meta.url).pathname, "src"),
             },
         },
     },
