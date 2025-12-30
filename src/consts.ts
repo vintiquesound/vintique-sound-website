4f17ffc6b426f52b455c5f5994d48f701b331b94
Kevin Ulliac <kevanoullio@outlook.com>
2025-12-30 15:04:41 -0700

    Improved and expanded layouts for web pages, added more title and description consts.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,13 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Vintique Sound';
-export const SITE_DESCRIPTION = 'Vintique Sound offers professional mixing and mastering services, educational resources for audio engineering and Cubase, a blog, audio plugins, loops and samples.';
+export const SITE_TITLE = "Vintique Sound";
+export const SITE_DESCRIPTION = "Vintique Sound offers professional mixing and mastering services, educational resources for audio engineering and Cubase, audio plugins, loops and samples, and a blog.";
+export const MIX_MASTER_TITLE = "Mixing and Mastering Services";
+export const MIX_MASTER_DESCRIPTION = "Professional mixing and mastering services for Indie Rock, Classic Rock, Hard Rock, House Music, and more. Unlimited revisions and personalized attention.";
+export const EDUCATION_TITLE = "Music Education: Mixing, Mastering & Cubase";
+export const EDUCATION_DESCRIPTION = "Learn professional mixing and mastering techniques, and master Steinberg Cubase with personalized lessons and resources.";
+export const BLOG_TITLE = "Blog | Vintique Sound";
+export const BLOG_DESCRIPTION = "The Vintique Sound blog is ";
+export const ABOUT_TITLE = "About | Vintique Sound";
+export const ABOUT_DESCRIPTION = "Learn about Vintique Sound, the services offered such as mixing/mastering, Cubase training, and the story behind the business."
