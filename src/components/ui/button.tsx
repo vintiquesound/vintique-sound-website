423dddb06c2102977fe8287611fbf1f37cecb9da
Kevin Ulliac <kevanoullio@outlook.com>
2026-01-02 13:08:40 -0700

    Consolidated all default styles in containers, improved style consistency, added three cards section.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all cursor-pointer hover:scale-103 disabled:pointer-events-none disabled:opacity-50 disabled:cursor-not-allowed [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border bg-background text-primary shadow-xs hover:text-primary hover:border-primary dark:bg-input/30 dark:border-foreground dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         accent:
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link:
-          "text-primary underline-offset-4 hover:underline",
+          "text-primary underline-offset-4 hover:underline hover:scale-100",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
@@ -45,6 +45,7 @@ function Button({
   variant = "default",
   size = "default",
   fullWidth = false,
+  usePointerCursor = true,
   icon,
   iconPosition = "left",
   asChild = false,
@@ -54,6 +55,7 @@ function Button({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
     fullWidth?: boolean
+    usePointerCursor?: boolean
     icon?: React.ReactNode
     iconPosition?: "left" | "right"
   }) {
@@ -80,7 +82,11 @@ function Button({
       data-slot="button"
       data-variant={variant}
       data-size={size}
-      className={cn(buttonVariants({ variant, size, className }), fullWidth && "w-full")}
+      className={cn(
+        buttonVariants({ variant, size, className }),
+        fullWidth && "w-full",
+        !usePointerCursor && "cursor-default"
+      )}
       {...props}
     >
       {content}
