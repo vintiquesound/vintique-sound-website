7ad2e9748c3e10f970279a8b97d95b5be11ddf2a
Kevin Ulliac <kevanoullio@outlook.com>
2026-01-02 13:37:11 -0700

    Cleaned up some button styling and made style code more readable instead of on one line.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,20 +1,26 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all cursor-pointer hover:scale-103 disabled:pointer-events-none disabled:opacity-50 disabled:cursor-not-allowed [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium \
+  transition-all cursor-pointer hover:scale-103 disabled:pointer-events-none disabled:opacity-50 \
+  disabled:cursor-not-allowed [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 \
+  shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 \
+  focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 \
+  aria-invalid:border-destructive",
   {
     variants: {
       variant: {
         default:
           "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
-          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 \
+          dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border bg-background text-primary shadow-xs hover:text-primary hover:border-primary dark:bg-input/30 dark:border-foreground dark:hover:bg-input/50",
+          "border bg-background text-foreground shadow-xs hover:text-primary hover:border-primary \
+          dark:bg-input/30 dark:border-foreground dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         accent:
@@ -46,6 +52,7 @@ function Button({
   size = "default",
   fullWidth = false,
   usePointerCursor = true,
+  useHoverScale = true,
   icon,
   iconPosition = "left",
   asChild = false,
@@ -56,6 +63,7 @@ function Button({
     asChild?: boolean
     fullWidth?: boolean
     usePointerCursor?: boolean
+    useHoverScale?: boolean
     icon?: React.ReactNode
     iconPosition?: "left" | "right"
   }) {
@@ -85,7 +93,8 @@ function Button({
       className={cn(
         buttonVariants({ variant, size, className }),
         fullWidth && "w-full",
-        !usePointerCursor && "cursor-default"
+        !usePointerCursor && "cursor-default",
+        !useHoverScale && "hover:scale-100"
       )}
       {...props}
     >
