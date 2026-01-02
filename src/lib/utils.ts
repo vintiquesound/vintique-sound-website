34e273d053aa325c79edebdb8bcd5f17574f1cba
Kevin Ulliac <kevanoullio@outlook.com>
2026-01-01 17:39:31 -0700

    Cleaned up blog post layout, refactored tags to use dedicated tag overview page.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function slugifyTag(tag: string) {
+  return tag
+    .trim()
+    .toLowerCase()
+    .replace(/[’']/g, "")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/(^-|-$)/g, "")
+}
